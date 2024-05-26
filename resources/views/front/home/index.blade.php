9730e6e257eba5ea9d0dcb152869d2cb113eb6a2
Thiago Correia <123599366+Thgocorreia@users.noreply.github.com>
2024-05-26 10:57:50 -0300

    n

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
 />
 <link rel="stylesheet" href="../style.css">
@@ -28,7 +29,7 @@
             <i class="fas fa-bars" id="menu-bars"></i>
             <i class="fas fa-search" id="search-icon"></i>
             <a href="#" class="fas fa-heart"></a>
-            <a href="#" class="fas fa-shopping-cart"></a>
+            <a href="carrinho.balde.php" class="fas fa-shopping-cart"></a>
         </div>
     </header>
 
@@ -191,6 +192,67 @@
 
 
 
+
+ <section class="carrinho">
+    <div class="cart">
+        <h1>Seu Carrinho</h1>
+        <div class="cart-item">
+            <img src="https://cdn-icons-png.freepik.com/256/2454/2454219.png?ga=GA1.1.969032829.1714322674&semt=ais_hybrid" alt="Produto 1">
+            <div class="item-details">
+                <h2>Produto 1</h2>
+                <p>Descrição do produto 1.</p>
+                <span class="price">$10.00</span>
+                <div class="quantity">
+                    <button class="decrement">-</button>
+                    <span class="quantity-number">1</span>
+                    <button class="increment">+</button>
+                </div>
+            </div>
+        </div>
+        <div class="cart-item">
+            <img src="https://cdn-icons-png.freepik.com/256/2454/2454219.png?ga=GA1.1.969032829.1714322674&semt=ais_hybrid" alt="Produto 2">
+            <div class="item-details">
+                <h2>Produto 2</h2>
+                <p>Descrição do produto 2.</p>
+                <span class="price">$20.00</span>
+                <div class="quantity">
+                    <button class="decrement">-</button>
+                    <span class="quantity-number">1</span>
+                    <button class="increment">+</button>
+                </div>
+            </div>
+        </div>
+        
+        <div class="carousel">
+        <div class="carousel-track">
+            <div class="carousel-item">
+                <img src="https://cdn-icons-png.freepik.com/256/2405/2405479.png?semt=ais_hybrid" alt="Bebida 1">
+                <h2>Bebida 1</h2>
+                <p>Descrição da bebida 1.</p>
+            </div>
+            <div class="carousel-item">
+                <img src="https://cdn-icons-png.freepik.com/256/4072/4072195.png?ga=GA1.1.969032829.1714322674&semt=ais_hybrid" alt="Bebida 2">
+                <h2>Bebida 2</h2>
+                <p>Descrição da bebida 2.</p>
+            </div>
+            <div class="carousel-item">
+                <img src="https://cdn-icons-png.freepik.com/256/2405/2405451.png?ga=GA1.1.969032829.1714322674&semt=ais_hybrid" alt="Bebida 3">
+                <h2>Bebida 3</h2>
+                <p>Descrição da bebida 3.</p>
+            </div>
+        </div>
+        <button class="carousel-button prev">←</button>
+        <button class="carousel-button next">→</button>
+    </div>
+
+    <div class="total">
+            <h2>Total: $30.00</h2>
+        </div>
+        <button class="checkout">Finalizar Compra</button>
+    </div>
+
+
+</section>
 
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
