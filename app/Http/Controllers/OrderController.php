c56c475965532ee2a482d7d28df58f0fe7837789
SamoelDuarte <129750291+SamoelDuarte@users.noreply.github.com>
2024-06-17 17:07:47 -0300

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -17,7 +17,7 @@ class OrderController extends Controller
 
     public function getOrders()
 {
-    $orders = Order::with(['customer', 'status'])->orderBy('id', 'desc')->get();
+    $orders = Order::with(['customer', 'status'])->orderBy('created_at', 'desc')->get();
 
     // Modificando os dados para incluir o atributo display_data
     foreach ($orders as $order) {
